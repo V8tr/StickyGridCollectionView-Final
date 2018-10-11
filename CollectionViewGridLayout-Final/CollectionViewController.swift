ba52fe76080b33ec296c5eec3a9039feb0f52ed1
V8tr <ne0_n@mail.ru>
2018-10-11 16:44:08 +0300

    Update project

--- CollectionViewGridLayout-Final/CollectionViewController.swift
+++ CollectionViewGridLayout-Final/CollectionViewController.swift
@@ -9,24 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    private enum Constants {
-        static let rowHeight: CGFloat = 50
-        static let columnsCount: CGFloat = 5
-    }
-    
-    @IBOutlet weak var gridCollectionView: UICollectionView! {
-        didSet {
-            gridCollectionView.bounces = false
-            gridCollectionView.dataSource = self
-            gridCollectionView.delegate = self
-        }
-    }
+
+	@IBOutlet weak var gridCollectionView: UICollectionView! {
+		didSet {
+			gridCollectionView.bounces = false
+		}
+	}
     
     @IBOutlet weak var gridLayout: StickyGridCollectionViewLayout! {
         didSet {
-            gridLayout.stickyRowsCount = 1
-            gridLayout.stickyColumnsCount = 1
+			gridLayout.stickyRowsCount = 1
+			gridLayout.stickyColumnsCount = 1
         }
     }
 }
@@ -38,19 +31,19 @@ extension ViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 100
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 100
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.reuseID, for: indexPath) as? CollectionViewCell else {
             return UICollectionViewCell()
         }
-        
+
         cell.titleLabel.text = "\(indexPath)"
-		cell.backgroundColor = gridLayout.isItemSticky(at: indexPath) ? .lightGray : .white
-        
+		cell.backgroundColor = gridLayout.isItemSticky(at: indexPath) ? .groupTableViewBackground : .white
+
         return cell
     }
 }
@@ -58,7 +51,6 @@ extension ViewController: UICollectionViewDataSource {
 extension ViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let columnWidth = collectionView.bounds.width / CGFloat(Constants.columnsCount)
-        return CGSize(width: columnWidth, height: Constants.rowHeight)
+        return CGSize(width: 100, height: 100)
     }
 }
--- CollectionViewGridLayout-Final/StickyGridCollectionViewLayout.swift
+++ CollectionViewGridLayout-Final/StickyGridCollectionViewLayout.swift
@@ -8,139 +8,82 @@
 
 import UIKit
 
-// Index Path Section = Row
-// Index Path Row (Item) = Column
 class StickyGridCollectionViewLayout: UICollectionViewFlowLayout {
 
-    var stickyRowsCount = 0 {
-        didSet {
-            invalidateLayout()
-        }
-    }
-    
-    var stickyColumnsCount = 0 {
-        didSet {
-            invalidateLayout()
-        }
-    }
+	var stickyRowsCount = 0 {
+		didSet {
+			invalidateLayout()
+		}
+	}
+
+	var stickyColumnsCount = 0 {
+		didSet {
+			invalidateLayout()
+		}
+	}
 
 	private var allAttributes: [[UICollectionViewLayoutAttributes]] = []
-	private var allSizes: [[CGSize]] = []
-	private var contentSize = CGSize.zero
-
-	// MARK: - Collection view flow layout methods
-
-	override func prepare() {
-		setupSizesIfNeeded()
-		setupAttributesIfNeeded()
-		updateStickyItemsPositions()
-
-		let lastSectionAttrs = allAttributes.last!
-		let lastItemAttrs = lastSectionAttrs.last!
-		let contentHeight = lastItemAttrs.frame.origin.y + lastItemAttrs.frame.height
-		let contentWidth = lastItemAttrs.frame.origin.x + lastItemAttrs.frame.width
-		contentSize = CGSize(width: contentWidth, height: contentHeight)
-	}
-
-    override var collectionViewContentSize: CGSize {
-        return self.contentSize
-    }
-    
-    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        return allAttributes[indexPath.section][indexPath.row]
-    }
-    
-    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        var layoutAttributes = [UICollectionViewLayoutAttributes]()
-        
-        for rowAttrs in allAttributes {
-            for itemAttrs in rowAttrs where rect.intersects(itemAttrs.frame) {
-				layoutAttributes.append(itemAttrs)
-            }
-        }
-        
-        return layoutAttributes
-    }
-    
-    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
-        return true
-    }
-    
-    override func invalidateLayout() {
-        super.invalidateLayout()
-        allAttributes = []
-        allSizes = []
-    }
-
-	// MARK: - Helpers
-
-	private var rowsCount: Int {
-		return collectionView!.numberOfSections
-	}
-
-	private func columnsCount(in row: Int) -> Int {
-		return collectionView!.numberOfItems(inSection: row)
-	}
 
 	func isItemSticky(at indexPath: IndexPath) -> Bool {
 		return indexPath.item < stickyColumnsCount || indexPath.section < stickyRowsCount
 	}
 
-	private func setupSizesIfNeeded() {
-		guard allSizes.isEmpty else {
-			return
-		}
-		for row in 0..<rowsCount {
-			var rowSizes: [CGSize] = []
-			for col in 0..<columnsCount(in: row) {
-				rowSizes.append(size(forRow: row, col: col))
-			}
-			allSizes.append(rowSizes)
-		}
+	// MARK: - Collection view flow layout methods
+
+	override var collectionViewContentSize: CGSize {
+		return CGSize(width: 10_000, height: 10_000)
 	}
 
-	private func setupAttributesIfNeeded() {
-		guard allAttributes.isEmpty else {
-			return
+	override func prepare() {
+		setupAttributes()
+		updateStickyItemsPositions()
+	}
+
+	override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+		var layoutAttributes = [UICollectionViewLayoutAttributes]()
+
+		for rowAttrs in allAttributes {
+			for itemAttrs in rowAttrs where rect.intersects(itemAttrs.frame) {
+				layoutAttributes.append(itemAttrs)
+			}
 		}
+
+		return layoutAttributes
+	}
+
+	override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+		return true
+	}
+
+	// MARK: - Helpers
+
+	private func setupAttributes() {
+		allAttributes = []
 
 		var xOffset: CGFloat = 0
 		var yOffset: CGFloat = 0
 
 		for row in 0..<rowsCount {
-			var lastItemSize = CGSize()
 			var rowAttrs: [UICollectionViewLayoutAttributes] = []
-			let interitemSpace = self.interitemSpace(forRow: row)
-			xOffset = insets(forRow: row).left
+			xOffset = 0
 
 			for col in 0..<columnsCount(in: row) {
-				lastItemSize = allSizes[row][col]
-				let indexPath = IndexPath(item: col, section: row)
-
+				let itemSize = size(forRow: row, column: col)
+				let indexPath = IndexPath(row: row, column: col)
 				let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
-				attributes.frame = CGRect(x: xOffset, y: yOffset, width: lastItemSize.width, height: lastItemSize.height).integral
-
-				if row < stickyRowsCount && col < stickyColumnsCount {
-					attributes.zIndex = ZOrder.staticStikyItem
-				} else if row < stickyRowsCount || col < stickyColumnsCount {
-					attributes.zIndex = ZOrder.stickyItem
-				}
+				attributes.frame = CGRect(x: xOffset, y: yOffset, width: itemSize.width, height: itemSize.height).integral
 
 				rowAttrs.append(attributes)
 
-				xOffset += lastItemSize.width + interitemSpace
+				xOffset += itemSize.width
 			}
-			yOffset += lastItemSize.height
+
+			yOffset += rowAttrs.last?.frame.height ?? 0.0
 			allAttributes.append(rowAttrs)
 		}
 	}
 
 	private func updateStickyItemsPositions() {
-		guard collectionView != nil else {
-			assertionFailure("Collection view cannot be nil")
-			return
-		}
-
 		for row in 0..<rowsCount {
 			for col in 0..<columnsCount(in: row) {
 				let attributes = allAttributes[row][col]
@@ -156,43 +99,55 @@ class StickyGridCollectionViewLayout: UICollectionViewFlowLayout {
 					frame.origin.x += collectionView!.contentOffset.x
 					attributes.frame = frame
 				}
+
+				attributes.zIndex = zIndex(forRow: row, column: col)
 			}
+		}
+	}
+
+	private func zIndex(forRow row: Int, column col: Int) -> Int {
+		if row < stickyRowsCount && col < stickyColumnsCount {
+			return ZOrder.staticStikyItem
+		} else if row < stickyRowsCount || col < stickyColumnsCount {
+			return ZOrder.stickyItem
+		} else {
+			return ZOrder.commonItem
 		}
 	}
 
 	// MARK: - Sizing
 
-	func size(forRow row: Int, col: Int) -> CGSize {
-		guard let delegate = collectionView?.delegate as? UICollectionViewDelegateFlowLayout else {
-			return .zero
-		}
+	private var rowsCount: Int {
+		return collectionView!.numberOfSections
+	}
 
-		guard let size = delegate.collectionView?(collectionView!, layout: self, sizeForItemAt: IndexPath(item: col, section: row)) else {
+	private func columnsCount(in row: Int) -> Int {
+		return collectionView!.numberOfItems(inSection: row)
+	}
+
+	private func size(forRow row: Int, column: Int) -> CGSize {
+		guard let delegate = collectionView?.delegate as? UICollectionViewDelegateFlowLayout,
+			let size = delegate.collectionView?(collectionView!, layout: self, sizeForItemAt: IndexPath(row: row, column: column)) else {
 			assertionFailure("Implement collectionView(_,layout:,sizeForItemAt: in UICollectionViewDelegateFlowLayout")
 			return .zero
 		}
 
-		return CGSize(width: floor(size.width), height: floor(size.height))
+		return size
 	}
+}
 
-	func insets(forRow row: Int) -> UIEdgeInsets {
-		guard let delegate = collectionView?.delegate as? UICollectionViewDelegateFlowLayout else {
-			return .zero
-		}
-		return delegate.collectionView?(collectionView!, layout: self, insetForSectionAt: row) ?? .zero
+// MARK: - IndexPath
+
+private extension IndexPath {
+	init(row: Int, column: Int) {
+		self = IndexPath(item: column, section: row)
 	}
+}
 
-	func interitemSpace(forRow row: Int) -> CGFloat {
-		guard let delegate = collectionView?.delegate as? UICollectionViewDelegateFlowLayout else {
-			return 0.0
-		}
-		return delegate.collectionView?(collectionView!, layout: self, minimumInteritemSpacingForSectionAt: row) ?? 0.0
-	}
+// MARK: - ZOrder
 
-	// MARK: - ZOrder
-
-	private enum ZOrder {
-		static let stickyItem = 1
-		static let staticStikyItem = 2
-	}
+private enum ZOrder {
+	static let commonItem = 0
+	static let stickyItem = 1
+	static let staticStikyItem = 2
 }
